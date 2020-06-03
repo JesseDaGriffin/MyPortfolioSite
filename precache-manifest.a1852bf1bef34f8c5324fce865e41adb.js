933c85885ea28cd213a2cbad878d515b3595ebe0
jessedagriffin <jesse.w.griffin@wsu.edu>
2020-06-02 21:29:03 -0700

    Updates

---
+++ precache-manifest.a1852bf1bef34f8c5324fce865e41adb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c1a75917e500ed6df905e1b6c9ea2d",
+    "url": "/MyPortfolioSite/index.html"
+  },
+  {
+    "revision": "d93f0f6886ceb4e94db7",
+    "url": "/MyPortfolioSite/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "db8176167c7206e2750c",
+    "url": "/MyPortfolioSite/static/css/main.1913e2d3.chunk.css"
+  },
+  {
+    "revision": "d93f0f6886ceb4e94db7",
+    "url": "/MyPortfolioSite/static/js/2.9481a405.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MyPortfolioSite/static/js/2.9481a405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8176167c7206e2750c",
+    "url": "/MyPortfolioSite/static/js/main.858c203c.chunk.js"
+  },
+  {
+    "revision": "c5afc43bda40e3bb800c",
+    "url": "/MyPortfolioSite/static/js/runtime-main.822dbad5.js"
+  },
+  {
+    "revision": "6f375282a32d6c9179363fdc0b6dffca",
+    "url": "/MyPortfolioSite/static/media/linkedinphoto.6f375282.jpg"
+  }
+]);
